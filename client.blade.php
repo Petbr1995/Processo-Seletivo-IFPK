eaa8674e3c2d284a942687aa3a61ff92ddcedd52
Peterson Brito <88500684+Petbr1995@users.noreply.github.com>
2021-10-04 20:56:38 -0400

    Create client.blade.php

---
+++ client.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registros do Cliente</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="client.css">
+    </head>
+<body>
+    <h1>Registros do Cliente </h1>
+
+    <table>
+  <tr>
+    <th>Nome Cliente</th>
+    <th>Descrição da Compra</th>
+    <th>Data da Compra</th>
+    <th>Valor da Compra</th>
+  </tr>
+  <tr>
+    <td> Peterson Brito</td>
+    <td>Smartphone</td>
+    <td>01/10/2021</td>
+    <td>2500.00</td>
+  </tr>
+
+  <tr>
+    <td>Suzanne Gomes </td>
+    <td>Secador de Cabelo</td>
+    <td>03/10/2021</td>
+    <td>150.00</td>
+  </tr>
+  <tr>
+    <td>Mayke Costa</td>
+    <td>Placa de video</td>
+    <td>12/09/2021</td>
+    <td>2500.00</td>
+  </tr>
+  <tr>
+    <td>Francisca Ozório</td>
+    <td>Tablet Samsung</td>
+    <td>20/09/2021</td>
+    <td>3000.00</td>
+  </tr>
+  
+</table>
+
+    <p id="link2"><a href="/"><button type="button" class="btn btn-outline-primary">Deseja voltar ao Menu Principal?</button></a></p>
+    
+
+</body>
+</html>
