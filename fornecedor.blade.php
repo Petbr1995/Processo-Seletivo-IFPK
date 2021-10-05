39b58832882fdb6300cfdb29a73f80a2758e84f4
Peterson Brito <88500684+Petbr1995@users.noreply.github.com>
2021-10-04 20:58:09 -0400

    Create fornecedor.blade.php

---
+++ fornecedor.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registros do fornecedor</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="registroFornecedor.css">
+    </head>
+<body>
+    <h1>Registros do fornecedor </h1>
+
+    <table>
+  <tr>
+    <th>Nome fornecedor</th>
+    <th>Descrição da Compra</th>
+    <th>Data da Compra</th>
+    <th>Valor da Compra</th>
+  </tr>
+  <tr>
+    <td>Samsung</td>
+    <td>Galaxy S21</td>
+    <td>08/08/2021</td>
+    <td>2000.00</td>
+  </tr>
+  <tr>
+    <td>GAMA</td>
+    <td>Secador</td>
+    <td>10/05/2021</td>
+    <td>100.00</td>
+  </tr>
+  <tr>
+    <td>Samsung</td>
+    <td>Tablet S21</td>
+    <td>02/09/2021</td>
+    <td>2000.00</td>
+  </tr>
+  <tr>
+    <td>AMD</td>
+    <td>Placa de Video</td>
+    <td>01/01/2021</td>
+    <td>1500.00</td>
+  </tr>
+  
+</table>
+
+    <p id="link2"><a href="/"><button type="button" class="btn btn-outline-primary">Deseja voltar ao Menu Principal?</button></a></p>
+    
+
+</body>
+</html>
