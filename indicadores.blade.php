5db35f7bf462e28ae41a3bfcb4b08de75b5123d1
Peterson Brito <88500684+Petbr1995@users.noreply.github.com>
2021-10-04 20:58:57 -0400

    Create indicadores.blade.php

---
+++ indicadores.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indicadores</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="indicadores.css">
+    </head>
+<body>
+    <h1>Indicadores Financeiros</h1>
+
+
+    <table>
+  <tr>
+    <th>Recebimentos</th>
+    <th>Pagamentos</th>
+    <th>Saldo</th>
+  </tr>
+  <tr>
+    <td>8150.00</td>
+    <td>6000.00</td>
+    <td>2150.00</td>
+  </tr>
+
+  </tr>
+</table>
+  
+
+    <p id="link2"><a href="/"><button type="button" class="btn btn-outline-primary">Deseja voltar ao Menu Principal?</button></a></p>
+    
+
+</body>
+</html>
