2452b66331862aeae3d387d72e6fa3b344c67b3b
Peterson Brito <88500684+Petbr1995@users.noreply.github.com>
2021-10-04 20:55:41 -0400

    Create welcome.blade.php

---
+++ welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Vendas</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="vendas.css">
+    </head>
+<body>
+    <form>
+    <h1>Sistema de Vendas</h1>
+
+    <p class="link"><a href="/cadastro" class="link"><button type="button" class="btn btn-outline-primary">Cadastro</button></a></p>
+
+    <p class="link"><a href="/client" class="link"><button type="button" class="btn btn-outline-primary">Clientes</button></a></p>
+
+    <p class="link"><a href="/fornecedor" class="link"><button type="button" class="btn btn-outline-primary">Fornecedores</button></a></p>
+
+    <p class="link"><a href="/indicadores" class="link"><button type="button" class="btn btn-outline-primary">Indicadores</button></a></p>
+
+    </form>
+</body>
+</html>
